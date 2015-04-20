c1192adfecc1e6998e72cca72793b468fc8c6e96
Rayron Victor <rayronvictor@gmail.com>
2015-04-20 14:46:04 -0300

    Using Bluebird Promise instead of callback style

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-var http = require('http'),
-    https = require('https'),
-    querystring = require('querystring');
+var querystring = require('querystring'),
+    rp = require('request-promise'),
+    Promise = require('bluebird');
 
 
 function InstagramClient(client_id, client_secret) {
@@ -14,134 +14,112 @@ function InstagramClient(client_id, client_secret) {
     this.users = new InstagramUsersClient(this);
 }
 
-InstagramClient.prototype.fetch = function (path, params, callback) {
-    if (arguments.length == 3) {
-	params.client_id = this.client_id;
-    }else{
-	var callback = params;
-	params = {client_id: this.client_id};
-    }
+InstagramClient.prototype.fetch = function (path, params) {
+
+    params.client_id = this.client_id;
 
     var options = {
-	host: 'api.instagram.com',
-	path: path+'?'+querystring.stringify(params),
-    }
+        url: 'https://api.instagram.com' + path + '?' + querystring.stringify(params)
+    };
 
-    https.get(options, function (res) {
-	var raw = "";
-	res.on('data', function (chunk) {
-	    raw += chunk;
-	});
-	res.on('end', function () {
-	    var response = JSON.parse(raw);
+    return new Promise(function(resolve, reject){
 
-	    var pagination = null;
-	    if (typeof(response['pagination']) != 'undefined') {
-		pagination = response['pagination'];
-	    }
+        rp(options)
+            .then(function(response){
+                var data = JSON.parse(response);
 
-	    if (response['meta']['code'] == 200) {
-		callback(response['data'], 
-			 null, 
-			 pagination);
-	    }else{
-		callback(response['meta'], response['meta']['code'], pagination);
-	    }
-	});
-    });    
-}
+                resolve({
+                    meta: data['meta'],
+                    data: data['data'],
+                    pagination: data['pagination']
+                });
+            })
+            .catch(function(err) {
+                reject(err);
+            });
+    });
+};
+
+//###### Media Client ######//
 
 function InstagramMediaClient(parent) {
     this.parent = parent;
 }
 
-InstagramMediaClient.prototype.id = function (id, callback) {
-    this.parent.fetch('/v1/media/'+id, callback);
+InstagramMediaClient.prototype.id = function (id, params) {
+    return this.parent.fetch('/v1/media/'+id, params);
 };
 
-InstagramMediaClient.prototype.popular = function (callback) {
-    this.parent.fetch('/v1/media/popular/', callback);
-}
+InstagramMediaClient.prototype.popular = function (params) {
+    return this.parent.fetch('/v1/media/popular/', params);
+};
 
-InstagramMediaClient.prototype.search = function (parameters, callback) {
-    this.parent.fetch('/v1/media/search/', parameters, callback);
-}
+InstagramMediaClient.prototype.search = function (params) {
+    return this.parent.fetch('/v1/media/search/', params);
+};
+
+//###### Tag Client ######//
 
 function InstagramTagsClient (parent) {
     this.parent = parent;
 }
 
-InstagramTagsClient.prototype.search = function (query, callback) {
-    this.parent.fetch('/v1/tags/search/',
-		      {q: query},
-		      callback);
-}
+InstagramTagsClient.prototype.search = function (query, params) {
+    params.q = query;
+    return this.parent.fetch('/v1/tags/search/', params);
+};
 
-InstagramTagsClient.prototype.media = function (tag, params, callback) {
-    if (arguments.length < 3) {
-	var callback = params;
-	params = {};
-    }
+InstagramTagsClient.prototype.media = function (tag, params) {
+    return this.parent.fetch('/v1/tags/'+tag+'/media/recent/', params);
+};
 
-    this.parent.fetch('/v1/tags/'+tag+'/media/recent/',
-		      params,
-		      callback);
-}
+InstagramTagsClient.prototype.tag = function (tag, params) {
+    return this.parent.fetch('/v1/tags/'+tag, params);
+};
 
-InstagramTagsClient.prototype.tag = function (tag, callback) {
-    this.parent.fetch('/v1/tags/'+tag, callback);
-}
+//###### Location Client ######//
 
 function InstagramLocationsClient (parent) {
     this.parent = parent;
 }
 
-InstagramLocationsClient.prototype.id = function (id, callback) {
-    this.parent.fetch('/v1/locations/'+id, callback);
-}
+InstagramLocationsClient.prototype.id = function (id, params) {
+    return this.parent.fetch('/v1/locations/'+id, params);
+};
 
-InstagramLocationsClient.prototype.media = function (id, params, callback) {
-    if (arguments.length < 3) {
-	var callback = params;
-	params = {};
-    }
+InstagramLocationsClient.prototype.media = function (id, params) {
+    return this.parent.fetch('/v1/locations/'+id+'/media/recent', params);
+};
 
-    this.parent.fetch('/v1/locations/'+id+'/media/recent',
-		      params,
-		      callback);
-}
+InstagramLocationsClient.prototype.search = function (params) {
+    return this.parent.fetch('/v1/locations/search', params);
+};
 
-InstagramLocationsClient.prototype.search = function (params, callback) {
-    this.parent.fetch('/v1/locations/search', params, callback);
-}
+//###### Users Client ######//
 
 function InstagramUsersClient (parent) {
     this.parent = parent;
 }
 
-InstagramUsersClient.prototype.id = function (id, callback) {
-    this.parent.fetch('/v1/users/'+id, callback);
-}
+InstagramUsersClient.prototype.id = function (id, params) {
+    return this.parent.fetch('/v1/users/'+id, params);
+};
 
-InstagramUsersClient.prototype.media = function (id, params, callback) {
-    this.parent.fetch('/v1/users/'+id+'/media/recent', params, callback);
-}
+InstagramUsersClient.prototype.media = function (id, params) {
+    return this.parent.fetch('/v1/users/'+id+'/media/recent', params);
+};
 
-InstagramUsersClient.prototype.self = function (params, callback) {
-    this.parent.fetch('/v1/users/self/feed', params, callback);
-}
+InstagramUsersClient.prototype.self = function (params) {
+    return this.parent.fetch('/v1/users/self/feed', params);
+};
 
-InstagramUsersClient.prototype.search = function (params, callback) {
-  if (typeof params == "string") {
-    params = {
-      q: params
-    }
-  }
-  this.parent.fetch('/v1/users/search/', params, callback);
-}
+InstagramUsersClient.prototype.search = function (query, params) {
+    params.q = query;
+    return this.parent.fetch('/v1/users/search/', params);
+};
 
 exports.createClient = function (client_id, client_secret) {
     var instagram_client = new InstagramClient(client_id, client_secret);
 
     return instagram_client;
-}
+};
